2b8e63498aaae3ef7030043b54511c3b308a3a0e
aliyuldashev <javohir.hbai@gmail.com>
2026-08-06 18:23:14 +0900

    feat: implement result fetching and scoring logic in BVController

--- src/bv_challenge/controller.py
+++ src/bv_challenge/controller.py
@@ -1,7 +1,10 @@
+import os
+
 import bittensor as bt
 
 from redteam_core.challenge_pool.controller import Controller
 from redteam_core.validator.models import MinerChallengeCommit
+import requests
 
 
 class BVController(Controller):
@@ -33,41 +36,73 @@ class BVController(Controller):
         self, miner_commit: MinerChallengeCommit, challenge_inputs
     ) -> None:
         _scoring_log = miner_commit.scoring_logs[0]
-        for i, miner_input in enumerate(challenge_inputs):
-
-            _higest_comparison_score = miner_commit.get_higest_comparison_score()
-            if (
-                _higest_comparison_score >= self.comparison_min_acceptable_score
-                or _higest_comparison_score == 0.0
-            ):
-                bt.logging.info(
-                    f"[CONTROLLER - MyController] Skipping scoring for miner {miner_commit.miner_hotkey} on task {i} "
-                    f"due to high comparison score: {_higest_comparison_score}"
-                )
-                _scoring_log.score = 0.0
-                if _scoring_log.error:
-                    _scoring_log.error += (
-                        " | Skipped scoring due to high comparison score."
-                    )
-                else:
-                    _scoring_log.error = "Skipped scoring due to high comparison score."
-                continue
-
-            score = (
-                self._score_challenge(
-                    miner_input=miner_input,
-                    miner_output=_scoring_log.miner_output,
-                    task_id=i,
-                )
-                if _scoring_log.miner_output is not None
-                else 0.0
+        _higest_comparison_score = miner_commit.get_higest_comparison_score()
+        if (
+            _higest_comparison_score >= self.comparison_min_acceptable_score
+            or _higest_comparison_score == 0.0
+        ):
+            bt.logging.info(
+                f"[CONTROLLER - ABSController] Skipping scoring for miner {miner_commit.miner_hotkey} on task "
+                f"due to high comparison score: {_higest_comparison_score}"
             )
+            _scoring_log.score = 0.0
+            if _scoring_log.error:
+                _scoring_log.error += " | Skipped scoring due to high comparison score."
+            else:
+                _scoring_log.error = "Skipped scoring due to high comparison score."
+            return
 
-            _scoring_log.score = score
+        score = (
+            self._score_challenge(
+                miner_input=challenge_inputs[0],
+                miner_output=_scoring_log.miner_output,
+                task_id=0,
+            )
+            if _scoring_log.miner_output is not None
+            else 0.0
+        )
 
+        _scoring_log.score = score
+        _result_response = self._get_results_from_challenge()
+        _scoring_log.miner_output["scoring_results"] = _result_response
+        # _scoring_log.miner_output["telemetry"] = self._get_telemetry_from_challenge()
         return
 
+    def _get_results_from_challenge(self) -> dict:
+        result_url = "http://localhost:10001/result"
+        api_key = os.environ.get("RT_CHALLENGE_API_KEY")
+        try:
+            response = requests.get(
+                result_url, timeout=5, verify=False, headers={"X-API-Key": api_key}
+            )  # nosec
+            response.raise_for_status()
+            _result_response = response.json() if response.content else {}
+            return _result_response
+        except Exception as exc:
+            bt.logging.error(
+                f"[CONTROLLER] Unable to fetch result from challenge endpoint: {exc}"
+            )
+            return {}
+
+    # def _get_telemetry_from_challenge(self) -> dict:
+    #     telemetry_url = "http://localhost:10001/telemetry"
+    #     try:
+    #         response = requests.get(telemetry_url, timeout=5, verify=False)  # nosec
+    #         response.raise_for_status()
+    #         return response.json() if response.content else {}
+    #     except Exception as exc:
+    #         bt.logging.error(
+    #             f"[CONTROLLER] Unable to fetch telemetry from challenge endpoint: {exc}"
+    #         )
+    #         return {}
+
     def _exclude_output_keys(self, miner_output: dict, reference_output: dict) -> None:
+        miner_output["commit_files"] = None
+        reference_output["commit_files"] = None
+        # miner_output["telemetry"] = None
+        # reference_output["telemetry"] = None
+        miner_output["scoring_results"] = None
+        reference_output["scoring_results"] = None
         return
 
 
